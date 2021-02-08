ac5d00b89229c8edcaa5e31cc62470fe74c32f37
arnaucube <root@arnaucube.com>
2021-02-08 23:05:21 +0100

    Add checks to inputs at Blind & BlindSign

--- README.md
+++ README.md
@@ -17,13 +17,15 @@ k, signerR := NewRequestParameters()
 
 // user: blinds the msg using signer's R
 msg := new(big.Int).SetBytes([]byte("test"))
-msgBlinded, userSecretData := Blind(msg, signerR)
+msgBlinded, userSecretData, err := Blind(msg, signerR)
+require.Nil(t, err)
 
 // signer: signs the blinded message using its private key & secret k
-sBlind := sk.BlindSign(msgBlinded, k)
+sBlind, err := sk.BlindSign(msgBlinded, k)
+require.Nil(t, err)
 
 // user: unblinds the blinded signature
-sig := Unblind(sBlind, msg, userSecretData)
+sig := Unblind(sBlind, userSecretData)
 
 // signature can be verified with signer PublicKey
 verified := Verify(msg, sig, signerPubK)
--- blindsecp256k1_test.go
+++ blindsecp256k1_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestFlow(t *testing.T) {
@@ -18,10 +19,12 @@ func TestFlow(t *testing.T) {
 
 	// user: blinds the msg using signer's R
 	msg := new(big.Int).SetBytes([]byte("test"))
-	msgBlinded, userSecretData := Blind(msg, signerR)
+	msgBlinded, userSecretData, err := Blind(msg, signerR)
+	require.Nil(t, err)
 
 	// signer: signs the blinded message using its private key & secret k
-	sBlind := sk.BlindSign(msgBlinded, k)
+	sBlind, err := sk.BlindSign(msgBlinded, k)
+	require.Nil(t, err)
 
 	// user: unblinds the blinded signature
 	sig := Unblind(sBlind, userSecretData)
@@ -34,3 +37,27 @@ func TestFlow(t *testing.T) {
 	verified := Verify(msg, sig, signerPubK)
 	assert.True(t, verified)
 }
+
+// func newBigIntWithBitLen(n int) *big.Int {
+//         b := make([]byte, n/8)
+//         for i := 0; i < len(b); i++ {
+//                 b[i] = 255
+//         }
+//         bi := new(big.Int).SetBytes(b[:])
+//         return bi
+// }
+//
+// func TestMinBigIntBytesLen(t *testing.T) {
+//         k := big.NewInt(1)
+//         sk := PrivateKey(*k)
+//
+//         mBlinded := newBigIntWithBitLen(MinBigIntBytesLen)
+//         require.Equal(t, MinBigIntBytesLen, mBlinded.BitLen())
+//         _, err := sk.BlindSign(mBlinded, k)
+//         assert.Nil(t, err)
+//
+//         mBlinded = new(big.Int).Div(mBlinded, big.NewInt(2))
+//         require.Equal(t, MinBigIntBytesLen-1, mBlinded.BitLen())
+//         _, err = sk.BlindSign(mBlinded, k)
+//         assert.Equal(t, "mBlinded too small", err.Error())
+// }
